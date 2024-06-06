7e999178e724253bb35bf4f9292f1dd51e97175b
skhodak <khod2@pdx.edu>
2024-06-06 07:15:17 +0000

    updated gitignore, added working virustotal URL info

--- app.py
+++ app.py
@@ -5,15 +5,19 @@ from langchain_community.agent_toolkits.load_tools import load_tools
 from langsmith import Client
 from textwrap import dedent
 from langchain import hub
+from dotenv import load_dotenv
+import asyncio
+import vt
+from pprint import pprint
 import traceback
 import os
 
+load_dotenv()
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = f"gensec-final"
 os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
 client = Client()
-
 
 gemini_llm = GoogleGenerativeAI(
     model="gemini-1.5-pro-latest",
@@ -26,11 +30,23 @@ gemini_llm = GoogleGenerativeAI(
 )
 gpt_llm = ChatOpenAI(model='gpt-4o', temperature=0)
 
+async def get_url_info(url):
+    api_key = os.getenv("VS_TOTAL_API_KEY")    
 
+    async with vt.Client(api_key) as client:
+        url_id = vt.url_id(url)
+        url = await client.get_object_async("/urls/{}", url_id)
+        print(type(url))
+        print(url)
+        pprint(vars(url))
 
 
 
 def main():
+
+    # TESTING 
+    asyncio.run(get_url_info("http://www.virustotal.com"))
+    # TESTING
 
     base_prompt = hub.pull("khodak/react-agent-template")
     prompt = base_prompt.partial(instructions=dedent("""You are an agent that is used for helping the user use any tool that's available to you.
@@ -48,6 +64,8 @@ def main():
 
     for tool in gpt_executor.tools:
         print(f"\n{tool.name}: \n\n\t{tool.description}")
+
+
     
 
     while True:
