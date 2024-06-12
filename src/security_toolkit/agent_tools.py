98e8ef7450dded8811d996395292329a3764f282
skhodak <khod2@pdx.edu>
2024-06-12 21:07:43 +0000

    Added get_analysis, moved validation of tools and updated them.

--- src/security_toolkit/agent_tools.py
+++ src/security_toolkit/agent_tools.py
@@ -1,10 +1,9 @@
-from .validating_models import UrlInput, FilenameInput
+from .validating_models import UrlInput, FilenameInput, ValidationError
 from langchain_core.tools import tool
 from pprint import pprint, pformat
 import vt
 from dotenv import load_dotenv
 import os
-import traceback
 import hashlib
 import requests
 import ujson
@@ -19,20 +18,30 @@ virustotal_api_key = os.getenv("VS_TOTAL_API_KEY")
 
 def collect_url_info(result):
     url_info = {
-        "category": result.url_categories,
-        "last_analysis_stats": result.last_analysis_stats,
-        "community_votes": result.total_votes
+        "category": result.get("categories", "None for this URL."),
+        "last_analysis_stats": result.get("last_analysis_stats", "None for this URL."),
+        "community_votes": result.get("total_votes", "None for this URL."),
     }
     return pformat(url_info, indent=2, sort_dicts=False)
 
 
 def collect_file_info(result):
     file_info = {
-        "names": result.names[:4],
-        "last_stats": result.last_analysis_stats,
-        "threat_class": result.popular_threat_classification,
+        "hash": result.get("sha256", None),
+        "names": result.get("names", [])[:4],
+        "last_stats": result.get("last_analysis_stats", "None for this file."),
+        "threat_class": result.get("popular_threat_classification", "None for this file."),
     }
     return pformat(file_info, indent=2, sort_dicts=False)
+
+
+def get_analysis(analysis_id, headers):
+    analysis_url = f"https://www.virustotal.com/api/v3/analyses/{analysis_id}"
+    analysis_response = requests.get(analysis_url, headers=headers)
+    analysis = ujson.loads(analysis_response.text)
+    status = analysis.get("data").get("attributes").get("status")
+
+    return status, analysis
 
 
 def upload_file(filename):
@@ -43,28 +52,37 @@ def upload_file(filename):
         "x-apikey": virustotal_api_key
     }
 
-    with open(f"./{filename}", "rb") as file:
-        files = { "file": (filename, file, "text/x-python") }
-        scan_response = requests.post(scan_url, files=files, headers=headers)
+    try:
+        with open(f"./{filename}", "rb") as file:
+            files = { "file": (filename, file, "text/x-python") }
+            scan_response = requests.post(scan_url, files=files, headers=headers)
+    except FileNotFoundError as error:
+        print(vars(error.strerror))
+        raise ValueError("File not found. Please make sure the file you're looking to scan is in the current directory.")
 
-    analysis_id = scan_response.json().get("data").get("id")
+
+    analysis_id = scan_response.json().get("data", {}).get("id")
     print(analysis_id)
-    
-    analysis_url = f"https://www.virustotal.com/api/v3/analyses/{analysis_id}"
-    analysis_response = requests.get(analysis_url, headers=headers)
-    analysis = ujson.loads(analysis_response.text)
-    status = analysis.get("data").get("attributes").get("status")
 
-    timeout = 300
+    # After posting file contents, we need to wait until the status code is "completed". 
+    # analysis_url = f"https://www.virustotal.com/api/v3/analyses/{analysis_id}"
+    # analysis_response = requests.get(analysis_url, headers=headers)
+    # analysis = ujson.loads(analysis_response.text)
+    # status = analysis.get("data").get("attributes").get("status")
+    status, analysis = get_analysis(analysis_id, headers)
+
+    timeout = 300  # Seconds
     interval = 5
     seconds = 0
+    # TODO: simplify, move to function?
     while((status != "completed") and (seconds < timeout)):
         print("Waiting for file scan to complete... Please standby.")
         sleep(interval)
         seconds += interval
-        analysis_response = requests.get(analysis_url, headers=headers)
-        analysis = ujson.loads(analysis_response.text)
-        status = analysis.get("data", {}).get("attributes", {}).get("status")
+        status, analysis = get_analysis(analysis_id, headers)
+        # analysis_response = requests.get(analysis_url, headers=headers)
+        # analysis = ujson.loads(analysis_response.text)
+        # status = analysis.get("data", {}).get("attributes", {}).get("status")
 
     if (status != "completed"):
         raise RuntimeError("The scan timed out. Due to unknown API delay, the file could not be scanned completely.")
@@ -76,12 +94,18 @@ def upload_file(filename):
 
 # ==== Tools ====
 
-@tool("check_url_safety", args_schema=UrlInput, return_direct=False)
+@tool("check_url_safety", return_direct=False)
 def check_url_safety(url):
     """
     Given a hostname URL, will return a concise dictionary of information on the security of the URL.
     """
-    url_id = vt.url_id(url)
+    try:
+        validated_url = UrlInput(url=url).url
+    except ValidationError as ve:
+        error = ve.errors()[0]
+        raise ValueError(error.get("msg")[13:])
+
+    url_id = vt.url_id(validated_url)
     with vt.Client(virustotal_api_key) as client:
         try:
             url_analysis = client.get_object("/urls/{}", url_id)
@@ -93,54 +117,54 @@ def check_url_safety(url):
             else:
                 raise
 
-        return collect_url_info(url_analysis)
+    return collect_url_info(url_analysis)
 
 
-@tool("get_file_info", args_schema=FilenameInput, return_direct=False)
+@tool("get_file_info", return_direct=False)
 def get_file_info(filename):
     """
     Given a filename, will get security information on whether the file is safe or not. The information will have potential
     malware names/hashes, stats on whether the file was rated as malicious, and the threat classification of the file once it is scanned.
     """
-
-    # TODO move this to pydantic v2 and test it here?
-    # os.path will return nothing if path ends with '/' on unix systems.
-    if (filename[-1] == '/' or filename[-1] == '\\'):
-        filename = filename[:-1]
-
-    final_filename = os.path.basename(filename)
-
-    if not final_filename:
-        raise ValueError("Invalid filename.")
+    try:
+        validated_filename = FilenameInput(file_name=filename).file_name
+    except ValidationError as ve:
+        error = ve.errors()[0]
+        raise ValueError(error.get("msg")[13:])
 
     # 128 kb in binary
     kb_120 = 131072 
     sha256_hasher = hashlib.new('sha256')
 
-    with open(f"./{final_filename}", "rb") as file:
+    try:
+        file = open(f"./{validated_filename}", "rb")
+    except FileNotFoundError:
+        raise ValueError("File not found. Please make sure the file you're looking to scan is in the current directory.")
 
-        # First, hash file to check if it's already in vt database.
-        while True:
-           bytes = file.read(kb_120)
-           if not bytes:
-               break
-           sha256_hasher.update(bytes)
-        file_hash = sha256_hasher.hexdigest()
+    # First, hash file to check if it's already in vt database.
+    while True:
+        bytes = file.read(kb_120)
+        if not bytes:
+            break
+        sha256_hasher.update(bytes)
+    file_hash = sha256_hasher.hexdigest()
 
-        # Request file info if it exists, and upload the file if not.
-        with vt.Client(apikey=virustotal_api_key, timeout=500) as client:
-            try: 
+    # Request file info if it exists, and upload the file if not.
+    with vt.Client(apikey=virustotal_api_key, timeout=500) as client:
+        try: 
+            file_analysis = client.get_object("/files/{}", file_hash)
+
+        except Exception as error:
+            if (error.code == "NotFoundError"):
+                print("\n\n\nThis file's hash is not in the VirusTotal database yet. Scanning - may take a minute.....\n\n\n")
+                file_hash = upload_file(validated_filename)                  
                 file_analysis = client.get_object("/files/{}", file_hash)
-
-            except Exception as error:
-                if (error.code == "NotFoundError"):
-                    print("\n\n\nThis file's hash is not in the VirusTotal database yet. Scanning - may take a minute.....\n\n\n")
-                    file_hash = upload_file(final_filename)                  
-                    file_analysis = client.get_object("/files/{}", file_hash)
-                else:
-                    raise
+            else:
+                raise
+    
+    file.close()
         
-        return collect_file_info(file_analysis)
+    return collect_file_info(file_analysis)
 
 
             
