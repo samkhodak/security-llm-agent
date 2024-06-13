11e5bea160fd0478cef4e00d73f3243c455db785
skhodak <khod2@pdx.edu>
2024-06-13 00:58:46 +0000

    fixed issues with file scanning, added retrieve_file_report

--- src/security_toolkit/agent_tools.py
+++ src/security_toolkit/agent_tools.py
@@ -7,6 +7,7 @@ import os
 import hashlib
 import requests
 import ujson
+import traceback
 from time import sleep
 
 
@@ -26,16 +27,17 @@ def collect_url_info(result):
 
 
 def collect_file_info(result):
+    attributes = result.get("data", {}).get("attributes", {})
     file_info = {
-        "hash": result.get("sha256", None),
-        "names": result.get("names", [])[:4],
-        "last_stats": result.get("last_analysis_stats", "None for this file."),
-        "threat_class": result.get("popular_threat_classification", "None for this file."),
+        "hash": attributes.get("sha256", None),
+        "names": attributes.get("names", [])[:4],
+        "last_stats": attributes.get("last_analysis_stats", "None for this file."),
+        "threat_class": attributes.get("popular_threat_classification", "None for this file."),
     }
     return pformat(file_info, indent=2, sort_dicts=False)
 
 
-def get_analysis(analysis_id, headers):
+def get_file_analysis(analysis_id, headers):
     analysis_url = f"https://www.virustotal.com/api/v3/analyses/{analysis_id}"
     analysis_response = requests.get(analysis_url, headers=headers)
     analysis = ujson.loads(analysis_response.text)
@@ -44,9 +46,24 @@ def get_analysis(analysis_id, headers):
     return status, analysis
 
 
+def retrieve_file_report(sha256):
+    url = f"https://www.virustotal.com/api/v3/files/{sha256}"
+    headers = {
+        "accept": "application/json",
+        "x-apikey": virustotal_api_key 
+    }
+
+    response = requests.get(url, headers=headers)
+    if (response.status_code == 404):
+        raise RuntimeError("This file's hash is not in the VirusTotal database.")
+        
+    file_report = ujson.loads(response.text)
+    return file_report 
+    
+
+
 def upload_file(filename):
     scan_url = "https://www.virustotal.com/api/v3/files"
-
     headers = {
         "accept": "application/json",
         "x-apikey": virustotal_api_key
@@ -56,39 +73,27 @@ def upload_file(filename):
         with open(f"./{filename}", "rb") as file:
             files = { "file": (filename, file, "text/x-python") }
             scan_response = requests.post(scan_url, files=files, headers=headers)
-    except FileNotFoundError as error:
-        print(vars(error.strerror))
+    except FileNotFoundError:
         raise ValueError("File not found. Please make sure the file you're looking to scan is in the current directory.")
 
 
     analysis_id = scan_response.json().get("data", {}).get("id")
-    print(analysis_id)
+    status, analysis = get_file_analysis(analysis_id, headers)
 
-    # After posting file contents, we need to wait until the status code is "completed". 
-    # analysis_url = f"https://www.virustotal.com/api/v3/analyses/{analysis_id}"
-    # analysis_response = requests.get(analysis_url, headers=headers)
-    # analysis = ujson.loads(analysis_response.text)
-    # status = analysis.get("data").get("attributes").get("status")
-    status, analysis = get_analysis(analysis_id, headers)
-
-    timeout = 300  # Seconds
+    timeout = 500  # Seconds
     interval = 5
     seconds = 0
-    # TODO: simplify, move to function?
     while((status != "completed") and (seconds < timeout)):
-        print("Waiting for file scan to complete... Please standby.")
+        print(f"Waiting for file scan to complete... Please standby. ({seconds} seconds)")
         sleep(interval)
         seconds += interval
-        status, analysis = get_analysis(analysis_id, headers)
-        # analysis_response = requests.get(analysis_url, headers=headers)
-        # analysis = ujson.loads(analysis_response.text)
-        # status = analysis.get("data", {}).get("attributes", {}).get("status")
+        status, analysis = get_file_analysis(analysis_id, headers)
 
     if (status != "completed"):
-        raise RuntimeError("The scan timed out. Due to unknown API delay, the file could not be scanned completely.")
+        raise RuntimeError("Due to API delay, your scan timed out. Please try again later.")
 
     file_info = analysis.get("meta", {}).get("file_info", {})
-    return file_info.get("sha_256")
+    return file_info.get("sha256")
 
 
 
@@ -103,7 +108,7 @@ def check_url_safety(url):
         validated_url = UrlInput(url=url).url
     except ValidationError as ve:
         error = ve.errors()[0]
-        raise ValueError(error.get("msg")[13:])
+        raise ValueError(error.get("msg")[13:]) # ValidationError returns a partly ugly string for our purposes.
 
     url_id = vt.url_id(validated_url)
     with vt.Client(virustotal_api_key) as client:
@@ -150,21 +155,17 @@ def get_file_info(filename):
     file_hash = sha256_hasher.hexdigest()
 
     # Request file info if it exists, and upload the file if not.
-    with vt.Client(apikey=virustotal_api_key, timeout=500) as client:
+    with vt.Client(apikey=virustotal_api_key, timeout=1000) as client:
         try: 
-            file_analysis = client.get_object("/files/{}", file_hash)
+            file_report = retrieve_file_report(file_hash)
 
-        except Exception as error:
-            if (error.code == "NotFoundError"):
-                print("\n\n\nThis file's hash is not in the VirusTotal database yet. Scanning - may take a minute.....\n\n\n")
-                file_hash = upload_file(validated_filename)                  
-                file_analysis = client.get_object("/files/{}", file_hash)
-            else:
-                raise
-    
+        except RuntimeError as exception:
+            print(f"\n\n{str(exception)} Scanning file - may take a minute...\n")
+            file_sha256 = upload_file(validated_filename)
+            file_report = retrieve_file_report(file_sha256) 
+
     file.close()
-        
-    return collect_file_info(file_analysis)
+    return collect_file_info(file_report)
 
 
             
